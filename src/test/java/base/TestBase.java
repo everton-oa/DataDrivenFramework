68d1bc9b2957d1cbe0e8011a79a16c27091a7c14
Everton Araujo <everton.oa@gmail.com>
2022-02-08 14:30:50 -0300

    Add log4j.properties file

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -3,10 +3,12 @@ package base;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -17,7 +19,15 @@ public class TestBase {
 
 	/*
 	 * 
-	 * WebDriver Properties Logs ExtentReports DB Excel Mail
+	 * WebDriver 
+	 * Properties 
+	 * Logs 
+	 * ExtentReports
+	 * ReportNG 
+	 * DB 
+	 * Excel 
+	 * Mail
+	 * Jenkins
 	 * 
 	 */
 
@@ -25,25 +35,51 @@ public class TestBase {
 	public static Properties config = new Properties();
 	public static Properties OR = new Properties();
 	public static FileInputStream fis;
+	public static Logger log = Logger.getLogger("devpinoyLogger");
 
 	@BeforeMethod
 	@BeforeSuite
-	public void setUp() throws IOException {
+	public void setUp() {
 		if (driver == null) {
-			fis = new FileInputStream(
-					System.getProperty("user.dir") + "/src/test/resources/properties/Config.properties");
-			config.load(fis);
+			try {
+				fis = new FileInputStream(
+						System.getProperty("user.dir") + "/src/test/resources/properties/Config.properties");
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				config.load(fis);
+				log.debug("Config file loaded");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
-			fis = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/properties/OR.properties");
-			OR.load(fis);
+			try {
+				fis = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/properties/OR.properties");
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				OR.load(fis);
+				log.debug("OR file loaded");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
 			if (config.getProperty("browser").equals("chrome")) {
 				driver = new ChromeDriver();
+				log.debug("Chrome launched");
 			} else if (config.getProperty("browser").equals("firefox")) {
 				driver = new FirefoxDriver();
+				log.debug("Firefox launched");
 			}
 		}
 		driver.get(config.getProperty("url"));
+		log.debug("URL opened");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")),
 				TimeUnit.SECONDS);
@@ -56,6 +92,6 @@ public class TestBase {
 		if (driver != null) {
 			driver.quit();
 		}
-
+		log.debug("Test execution completed");
 	}
 }
--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -9,8 +9,10 @@ public class LoginTest extends TestBase{
 	
 	@Test
 	public void login() throws InterruptedException {
+		log.debug("Starting login test");
 		driver.findElement(By.cssSelector(OR.getProperty("bankManagerLoginBtn"))).click();
 		Thread.sleep(2000);
+		log.debug("Login test successfully executed");
 		
 	}
 
