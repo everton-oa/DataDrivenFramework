99cd3dd8557cb42d7c2638c23908201bc6058076
Everton Araujo <everton.oa@gmail.com>
2022-02-10 06:02:51 -0300

    Add testNG reports feature

--- src/test/java/testcases/BankManagerLoginTest.java
+++ src/test/java/testcases/BankManagerLoginTest.java
@@ -2,6 +2,7 @@ package testcases;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import base.TestBase;
@@ -10,11 +11,14 @@ public class BankManagerLoginTest extends TestBase{
 	
 	@Test
 	public void login() throws InterruptedException {
+		System.setProperty("org.uncommons.reportng.escape-output", "false");
 		log.debug("Starting login test");
 		driver.findElement(By.cssSelector(OR.getProperty("bankManagerLoginBtn"))).click();
 		Thread.sleep(2000);
 		Assert.assertTrue(isElementPresent(By.cssSelector(OR.getProperty("addCustomerBtn"))), "Could not find addCustomerBtn");
 		log.debug("Login test successfully executed");
+		Reporter.log("Login test successfully executed");
+		Reporter.log("<a target=\"_blank\" href=\"/home/everton/Pictures/montanha.jpg\">Screenshot</a>");
 		
 	}
 
