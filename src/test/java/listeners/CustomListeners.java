aa702644cd5d45266b825cd16e16874e00ef2e28
Everton Araujo <everton.oa@gmail.com>
2022-02-11 04:56:58 -0300

    Add listeners feature

---
+++ src/test/java/listeners/CustomListeners.java
@@ -0,0 +1,53 @@
+package listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+public class CustomListeners implements ITestListener {
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		System.setProperty("org.uncommons.reportng.escape-output", "false");
+		Reporter.log("Test failed - Capturing screenshot");
+		Reporter.log("<a target=\"_blank\" href=\"/home/everton/Pictures/montanha.jpg\">Screenshot</a>");
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
