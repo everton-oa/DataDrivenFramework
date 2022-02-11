17ddcb997647e272d8434550064f7e2ac8c28cf6
Everton Araujo <everton.oa@gmail.com>
2022-02-11 05:52:47 -0300

    Add screenshot capture feature

--- src/test/java/listeners/CustomListeners.java
+++ src/test/java/listeners/CustomListeners.java
@@ -1,9 +1,13 @@
 package listeners;
 
+import java.io.IOException;
+
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
+
+import utilities.TestUtil;
 
 public class CustomListeners implements ITestListener {
 
@@ -22,8 +26,16 @@ public class CustomListeners implements ITestListener {
 	@Override
 	public void onTestFailure(ITestResult result) {
 		System.setProperty("org.uncommons.reportng.escape-output", "false");
+		try {
+			TestUtil.captureScreenShot();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Reporter.log("Test failed - Capturing screenshot");
-		Reporter.log("<a target=\"_blank\" href=\"/home/everton/Pictures/montanha.jpg\">Screenshot</a>");
+		Reporter.log("<a target=\"_blank\" href=\""+TestUtil.screenShotName+"\">Screenshot</a>");
+		Reporter.log("<br>");
+		Reporter.log("<a target=\"_blank\" href=\""+TestUtil.screenShotName+"\"><img src="+TestUtil.screenShotName+" height=200 width=200></img></a>");
 	}
 
 	@Override
