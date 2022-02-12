eed3171b9b65dd354a38fd56b07b6a0ca97d8989
Everton Araujo <everton.oa@gmail.com>
2022-02-12 06:23:21 -0300

    Refactor dataprovider use

--- src/test/java/testcases/AddCustomerTest.java
+++ src/test/java/testcases/AddCustomerTest.java
@@ -3,14 +3,14 @@ package testcases;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import base.TestBase;
+import utilities.TestUtil;
 
 public class AddCustomerTest extends TestBase{
 	
-	@Test(dataProvider="getData")
+	@Test(dataProviderClass=TestUtil.class, dataProvider = "dp")
 	public void addCustomerTest(String firstName, String lastName, String postCode, String alertText) {
 		click("addCustomerBtn_CSS");
 		type("firstNameField_CSS", firstName);
@@ -23,19 +23,4 @@ public class AddCustomerTest extends TestBase{
 		Assert.assertTrue(alert.getText().contains(alertText));
 		alert.accept();
 	}
-	
-	@DataProvider
-	public Object[][] getData() {
-		String sheetName = "AddCustomerTest";
-		int rows = excel.getRowCount(sheetName);
-		int cols = excel.getColumnCount(sheetName);
-		Object[][] data = new Object[rows-1][cols];
-		for (int rowNum=2; rowNum<=rows; rowNum++) {
-			for (int colNum=0; colNum<cols; colNum++) {
-				data[rowNum-2][colNum] = excel.getCellData(sheetName, colNum, rowNum);
-			}
-		}
-		return data;
-	}
-
 }
--- src/test/java/utilities/TestUtil.java
+++ src/test/java/utilities/TestUtil.java
@@ -2,24 +2,41 @@ package utilities;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.testng.annotations.DataProvider;
 
 import base.TestBase;
 
 public class TestUtil extends TestBase {
-	
+
 	public static String screenShotPath;
 	public static String screenShotName;
 
 	public static void captureScreenShot() throws IOException {
 		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		Date currentDate = new Date();
-		screenShotName = currentDate.toString().replace(":", "_").replace(" ", "_")+".jpg";
-		FileUtils.copyFile(scrFile, new File(System.getProperty("user.dir")+"/target/surefire-reports/html/"+screenShotName));
+		screenShotName = currentDate.toString().replace(":", "_").replace(" ", "_") + ".jpg";
+		FileUtils.copyFile(scrFile,
+				new File(System.getProperty("user.dir") + "/target/surefire-reports/html/" + screenShotName));
+	}
+
+	@DataProvider(name = "dp")
+	public Object[][] getData(Method method) {
+		String sheetName = method.getName();
+		int rows = excel.getRowCount(sheetName);
+		int cols = excel.getColumnCount(sheetName);
+		Object[][] data = new Object[rows - 1][cols];
+		for (int rowNum = 2; rowNum <= rows; rowNum++) {
+			for (int colNum = 0; colNum < cols; colNum++) {
+				data[rowNum - 2][colNum] = excel.getCellData(sheetName, colNum, rowNum);
+			}
+		}
+		return data;
 	}
 
 }
